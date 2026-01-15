3a40f2409437d0e089d0ae993b43fb770b9331f6
Bui Gia Han <hanbuii7705@gmail.com>
2026-01-15 13:48:31 +0700

    Lưu thông tin bước chơi

---
+++ GameCaro/Playinfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCaro
+{
+    public class Playinfo
+    {
+        private Point point;
+		public Point Point { get => point; set => point = value; }
+
+		private  int currentPlayer;
+		public int CurrentPlayer { get => currentPlayer; set => currentPlayer = value; }
+
+		public Playinfo(Point point , int currentPlayer) {
+			this.Point = point;
+			this.CurrentPlayer = currentPlayer;
+		}
+	}
+}
